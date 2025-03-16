50e68bfd34eaee9cf2d02ddcbe2bcbc3b4ea27ce
Ivan Plesovskikh <plesovskikh.ivan@gmail.com>
2025-03-16 13:50:06 +0300

    fix crash-test

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -14,6 +14,12 @@ const router = Router();
 router.post('/signup', validateUserBody, createUser);
 router.post('/signin', validateAuthentication, login);
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 // все роуты, кроме /signin и /signup, защищены авторизацией;
 router.use(auth);
 router.use('/users', userRouter);
